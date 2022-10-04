2beed186e03749005feeb61dce182770537685aa
brivanessa <vanessaah.lu@gmail.com>
2022-10-03 19:18:11 -0500

    creando component Login con formato css

--- my-app/src/components/login.jsx
+++ my-app/src/components/login.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import './login.css'
 
 export const Login = () => {
-  return (
-    <div>
-      Login
+    return (  //return html y antes de return logica js
+    <div className='viewLogin'>
+            <div className='spaceLogin'>
+                <div className="logoFastFood">
+                    <img src={logo} alt="logo"></img>
+                </div>
+                <div className='areaDatosLogin'>
+                    <p className='instructions'>Escribe tu usuario y contraseña para iniciar sesión.</p>
+                    <div className='email'>
+                        {/* <img src={require('../assets/usuario.png')} alt="usuario" className='usuarioImagen'></img> */}
+                        <input type='email' placeholder='Correo electrónico' className='loginEmail'></input>
+                    </div>
+                    <div className='password'>
+                        {/* <img src='../assets/contraseña.png' alt="contraseña"></img> */}
+                        <input type='password' placeholder='Contraseña' className='loginPassword'></input>
+                    </div>
+                </div>
+            </div> 
     </div>
-  )
+    );
 }
 
-// export default Login
