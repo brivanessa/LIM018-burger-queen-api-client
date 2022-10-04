7a4ca7562cf4e924b9158ed5746ace458d3c2bde
brivanessa <vanessaah.lu@gmail.com>
2022-10-03 19:00:18 -0500

    instalando BrowserRouter

--- my-app/src/components/Menu.jsx
+++ my-app/src/components/Menu.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export const Login = () => {
+const Menu = () => {
   return (
     <div>
-      Login
+      
     </div>
   )
 }
 
-// export default Login
+export default Menu
