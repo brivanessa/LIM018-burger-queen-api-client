ad3224172ab8b7589aa644a6815b9b20c3591e06
brivanessa <vanessaah.lu@gmail.com>
2022-10-04 00:04:12 -0500

    usando Navlink

---
+++ my-app/src/components/WEntregados.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const WEntregados = () => {
+  return (
+    <div>
+      Pedidos Entregados
+    </div>
+  )
+}
+
+// export default WEntregados
---
+++ my-app/src/components/WPendientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const WPendientes = () => {
+  return (
+    <div>
+      Pedidos pendientes
+    </div>
+  )
+}
+
+// export default WPendientes
