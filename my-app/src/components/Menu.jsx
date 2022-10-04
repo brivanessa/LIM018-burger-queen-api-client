9fb6bbe390b10b85acdb1e726362d7d18c7dd2de
brivanessa <vanessaah.lu@gmail.com>
2022-10-03 23:04:53 -0500

    usando Navlink

--- my-app/src/components/Menu.jsx
+++ my-app/src/components/Menu.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export const Menu = () => {
+export const Menu = (props) => {
   return (
     <div className="optionsMenu">
-      <NavLink to="" className="nav-link-TomarPedido">Tomar Pedidos</NavLink>
-      <NavLink to="" className="nav-link-Pendientes">Pedidos Pendientes</NavLink>
-      <NavLink to="" className="nav-link-Preparados">Pedidos Preparados</NavLink>
-      <NavLink to="" className="nav-link-Entregados">Pedidos Entregadosr</NavLink>
+      <div>Hola: {props.usuario}</div>
+      <nav>
+      {/* <ul> */}
+      <li><NavLink to="" className="nav-link-TomarPedido">Tomar Pedidos</NavLink></li>
+      <li><NavLink to="" className="nav-link-Pendientes">Pedidos Pendientes</NavLink></li>
+      <li><NavLink to="" className="nav-link-Preparados">Pedidos Preparados</NavLink></li>
+      <li><NavLink to="" className="nav-link-Entregados">Pedidos Entregadosr</NavLink></li>
 
+      {/* </ul> */}
+      </nav>
     </div>
   )
 }
--- my-app/src/components/login.jsx
+++ my-app/src/components/login.jsx
@@ -1,21 +1,37 @@
 import React, {useState} from 'react'
 import logo from '../assets/logo.png'
 import './login.css'
-import { Menu } from './Menu'
+import { Menu } from './Menu.jsx'
 
 export const Login = () => {
-    const [miLogin, setLogin] = useState("false");
+    const [myLogin, setLogin] = useState("false");
     const [correo, setCorreo] = useState();
     const [password, setPassword] = useState();
 
     function iniciarSesion(e){
         e.preventDefault();
-
+        const txtEmail = document.getElementById("emailInput").value;
+        const txtPassword = document.getElementById("passwordInput").value;
+        if(txtEmail.length===0 || txtPassword.length===0 ){
+            alert("Completa tus datos...");
+        } else {
+            if(correo==="123" && password==="123" ){
+              setLogin("true");
+              document.getElementById("viewLogin").style.display ="none"
+            }else{
+                setLogin("false");
+                alert("El usuario o contraseña son incorrectos...");
+                document.getElementById("emailInput").value="";
+                document.getElementById("passwordInput").value="";
+                document.getElementById("emailInput").focus();
+            }
+        }
     }
 
 
     return (  //return html y antes de return logica js
-    <div className='viewLogin'>
+    <>
+        <div className='viewLogin' id='viewLogin'>
             <div className='spaceLogin'>
                 <div className="logoFastFood">
                     <img src={logo} alt="logo"></img>
@@ -30,10 +46,13 @@ export const Login = () => {
                         {/* <img src='../assets/contraseña.png' alt="contraseña"></img> */}
                         <input type='password' id='passwordInput' placeholder='Contraseña' className='loginPassword' onChange={(e)=>setPassword(e.target.value)}></input>
                     </div>
-                    <input type="submit" className="btnLogin"></input>
+                    <input type="submit" className="btnLogin" onClick={iniciarSesion}></input>
                 </div>
             </div> 
     </div>
+    { myLogin==="true" && <Menu usuario={correo}/> } 
+    </>
+
     );
 }
 
