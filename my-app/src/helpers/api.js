9a43b9401ab543f2c4e590f238855fc150e5be50
brivanessa <vanessaah.lu@gmail.com>
2022-10-05 18:19:52 -0500

    creando path y rutas

---
+++ my-app/src/helpers/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:3001"
+
+const authPath = "/auth"
+
+export const auth = (email,password) => {
+    return axios.post(`${BASE_URL}${authPath}`,{email,password})
+}
