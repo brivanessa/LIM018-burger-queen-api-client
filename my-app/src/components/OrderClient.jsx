ed9070c9381fad70d1ef4449a634ca6dfb7b1cab
ArkellyPerez <arkelly.perez.alayo@gmail.com>
2022-10-24 10:04:37 -0500

    Agregando items al pedido

--- my-app/src/components/OrderClient.jsx
+++ my-app/src/components/OrderClient.jsx
@@ -1,12 +1,22 @@
 import { useState, useEffect } from 'react';
+import React from 'react';
 
+//export const OrderClient = (props) => {
+ export const SkillList = React.memo(({ skills }) => {
+//   return(
+//   <div className="OrderClientList">
 
-export const OrderClient = (props) => {
+//     <div className="productName">{props.producto}</div>
+//   </div>
+// )
+ 
+return (
+  <ul>
+    {skills.map((skill, i) => (
+      <li key={i}>{skill}</li>
+    ))}
+  </ul>
 
-  return(
-  <div className="OrderClientList">
-
-    <div className="productName">{props.producto}</div>
-  </div>
 )
-}
+
+})
--- my-app/src/pages/Menu.jsx
+++ my-app/src/pages/Menu.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { productsGet } from '../helpers/api'
-import { OrderClient } from '../components/OrderClient';
-
-
-
 
 export const Menu = () => {
   const [productosMenuArray, setProductosMenuArray] = useState([])
   const tokenSaved = localStorage.getItem('llave')
-
-  const [OrderArray, setOrderArray] = useState()
-  let prueba = [];
-
+  const [OrderArray, setOrderArray] = useState([])
+  // const [TotalOrder, setTotalOrder] = useState(0)
+  let totalOrder = 0;
   useEffect(() => {
     productsGet(tokenSaved)
       .then((res) => {
@@ -27,17 +22,61 @@ export const Menu = () => {
 
   //-----------Añadir productos al pedido------------------
 
-  //   const ClientOrderAdd = (producto) => {
-  //      console.log(producto.name);
-  //      return
-  //   //    setOrderArray(producto.name)
-  //   //  console.log( setOrderArray(producto.name));
+  const ClientOrderAdd = (producto) => {
+    //  setOrderArray([...OrderArray,  producto.name]);
+    const existInOrder = OrderArray.find((item) => {
+      return item._id === producto._id
+    })
+    if (existInOrder) {
+      const addQuantityItem = OrderArray.map((item) => {
+        if (item._id === producto._id) {
 
-  //   }
-  // setOrderArray(ClientOrderAdd)
 
+          return { ...producto, quantity: item.quantity + 1, price: item.price + producto.price }
+        }
+        return item;
+      })
+      setOrderArray(addQuantityItem)
 
+    } else {
 
+      setOrderArray([...OrderArray, { ...producto, quantity: 1 }]);
+
+    }
+  }
+
+  // const TotalAmountOrder = () => {
+  OrderArray.map((product, i) => {
+    const item = product;
+    totalOrder += item.price;
+    console.log(totalOrder)
+ 
+  })
+  // setTotalOrder(totalOrder1)
+  //}
+
+  const deleteItemOrder = (product) => {
+    const existInOrder = OrderArray.find((value) => {
+      return value._id === product._id
+    });
+
+    if (existInOrder.quantity === 1) {
+      setOrderArray(
+        OrderArray.filter(elementInCar => elementInCar._id !== product._id)
+      )
+    } else if (existInOrder.quantity > 1) {
+      console.log("product.price")
+      const newArray = OrderArray.map((element) => {
+        if (element._id === product._id) {
+          console.log(product.price)
+          return { ...product, quantity: element.quantity - 1, price: element.price - product.price }
+
+        }
+        return element;
+      })
+      setOrderArray(newArray)
+    }
+  };
 
 
   return (
@@ -50,22 +89,20 @@ export const Menu = () => {
         <div className="waiterContainer">
           <div className="menuConstainer">
             {productosMenuArray.map((producto, index) => (
-              <div className="product" key={index}  onClick={(e) => {
-                console.log(e.target)
-                // prueba=producto.name
+              <div className="product" key={index} onClick={(e) => {
+                // console.log(e.target)
+
               }}>
                 <div className="productName">{producto.name}</div>
                 <div className="productCost">`S/{producto.price}`</div>
                 <div className="imgProduct">
                   <img src={producto.image} alt="sanwich"></img>
                 </div>
-                <div className="buttonAñadir">+ Añadir
-                  <button data-id={producto._id} className="buttonAñadir" onClick={() => {
-                   console.log(producto.name)
-                   console.log(prueba)
-
-                  return prueba.push(producto.name)
-                  }}></button>
+                <div className="buttonAñadir" onClick={() => {
+                  ClientOrderAdd(producto);
+                  //TotalAmountOrder()
+                }
+                }>+ Añadir
                 </div>
               </div>
             )
@@ -75,12 +112,37 @@ export const Menu = () => {
       </div>
       <div className="waiterList">
         <h1>DETALLE DEL PEDIDO</h1>
-        <OrderClient producto="Helado" />
-        
-        <OrderClient producto={prueba} />
+        <table className="TableOrder">
+          <thead>
+            <tr className="rowHead">
+              <th className="rowHeadTable">Cant.</th>
+              <th className="rowHeadTable">Producto</th>
+              <th className="rowHeadTable">S/.</th>
+              <th className="rowHeadTable"> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {OrderArray.map((product, i) => (
+              <tr key={i}>
+                <td className="itemsOrderTable">{product.quantity}</td>
+                <td className="itemsOrderTable">{product.name}</td>
+                <td className="itemsOrderTable">{product.price}</td>
+                <td>
+                  <button className="Btn-delete-item-product" type="button" onClick={() => deleteItemOrder(product)}>Elidddd
+                    {/* <img   alt="buttonDelete" className="Image-button-delete" /> */}
+                  </button>
+                </td>
+              </tr>
+            ))}
+            <tr>
+              <td>
+              </td>
+              <th className="itemsOrderTable">TotalPedido   S/.</th>
+              <td className="itemsOrderTable">{totalOrder}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
-
     </div>
   );
 
